c5d9a0c678f42bfa3e7526ea6b0362bd8dbe1ba4
Mariell Hoversholm <proximyst@proximyst.com>
2020-12-17 16:20:35 +0100

    1.16.4

--- script.sh
+++ script.sh
@@ -5,13 +5,13 @@ apt-get update
 apt-get install -qy wget
 echo "$MVN_SETTINGS" > mvn-settings.xml
 wget https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
-java -jar BuildTools.jar --rev 1.16.3
+java -jar BuildTools.jar --rev 1.16.4
 mvn deploy:deploy-file \
     -s ./mvn-settings.xml \
     -DgroupId=org.spigotmc \
     -DartifactId=spigot \
-    -Dversion=1.16.3-R0.1-SNAPSHOT \
+    -Dversion=1.16.4-R0.1-SNAPSHOT \
     -Dpackaging=jar \
-    -Dfile=spigot-1.16.3.jar \
+    -Dfile=spigot-1.16.4.jar \
     -DrepositoryId=proxi-nexus \
     -Durl=https://nexus.proximyst.com/repository/maven-any/
